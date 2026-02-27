d217686fbbf2d660ab10e88b71a742cfde8ca0b4
MinhTuann <vuminhtuan1701@gmail.com>
2026-02-27 14:13:08 +0700

    fix: enhance header scroll behavior and responsiveness layout

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import { Link } from 'react-router-dom';
 import IconButton from './IconButton';
 import { MenuIcon, WatchIcon, SearchIcon } from 'lucide-react';
@@ -9,26 +9,32 @@ interface Props {
 }
 
 function Header({ scrollY }: Props) {
+    const viewRef = useRef<HTMLDivElement>(null);
     const [isScrolledOutOfVideo, setIsScrolledOutOfVideo] = useState(false);
 
-    useMotionValueEvent(scrollY, "change", (current) => {
-        setIsScrolledOutOfVideo(current > window.innerHeight)
+    useMotionValueEvent(scrollY, 'change', (current) => {
+        setIsScrolledOutOfVideo(current > window.innerHeight - (viewRef.current?.offsetHeight || 0))
     })
 
     return (
-        <header className={`fixed w-full ${isScrolledOutOfVideo ? 'bg-white' : 'bg-transparent'}`}>
-            <div className="mx-auto px-6 h-16 flex items-center justify-between">
-                {/* text */}
-                <IconButton icon={<MenuIcon strokeWidth={1} />} label={'Menu'} className={isScrolledOutOfVideo ? 'text-black' : 'text-white'} />
-                {/* logo */}
-                <Link to="/" className='flex items-center gap-2'>
-                    <img src={`${import.meta.env.BASE_URL}logo_${isScrolledOutOfVideo ? 'black' : 'white'}.png`} alt="logo" className='h-32' />
-                </Link>
-                {/* menu */}
-                <nav className='flex gap-6'>
-                    <IconButton icon={<WatchIcon strokeWidth={1} />} label={'Products'} className={isScrolledOutOfVideo ? 'text-black' : 'text-white'} />
-                    <IconButton icon={<SearchIcon strokeWidth={1} />} label={'Search'} className={isScrolledOutOfVideo ? 'text-black' : 'text-white'} />
-                </nav>
+        <header
+            ref={viewRef}
+            className={`fixed top-0 z-50 w-[100dvw] transition-colors duration-300 ${isScrolledOutOfVideo ? 'bg-white' : 'bg-transparent'} items-center`}>
+            <div className='mx-auto px-6 grid grid-cols-3 items-center justify-between'>
+                <div className='flex justify-start'>
+                    <IconButton icon={<MenuIcon strokeWidth={1} />} label={'Menu'} className={isScrolledOutOfVideo ? 'text-black' : 'text-white'} />
+                </div>
+                <div className='flex justify-center'>
+                    <Link to="/" className='flex items-center'>
+                        <img src={`${import.meta.env.BASE_URL}logo_${isScrolledOutOfVideo ? 'black' : 'white'}.png`} alt="logo" className='h-16 md:h-18' />
+                    </Link>
+                </div>
+                <div className='flex justify-end'>
+                    <nav className='flex gap-6'>
+                        <IconButton icon={<WatchIcon strokeWidth={1} />} label={'Products'} className={isScrolledOutOfVideo ? 'text-black' : 'text-white'} />
+                        <IconButton icon={<SearchIcon strokeWidth={1} />} label={'Search'} className={isScrolledOutOfVideo ? 'text-black' : 'text-white'} />
+                    </nav>
+                </div>
             </div>
         </header>
     )
